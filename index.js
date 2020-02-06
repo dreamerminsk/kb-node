02d3ad90f2b6a1d1b6bc33cf18e50c325486930f
dreamerminsk <David70908019Miller>
2020-02-06 09:56:09 +0300

    koa start template

---
+++ index.js
@@ -0,0 +1,20 @@
+var koa = require('koa');
+var router = require('koa-router');
+var bodyParser = require('koa-body');
+
+var app = koa();
+
+//Set up body parsing middleware
+app.use(bodyParser({
+   formidable:{uploadDir: './uploads'},
+   multipart: true,
+   urlencoded: true
+}));
+
+//Require the Router we defined in movies.js
+var movies = require('./movies.js');
+
+//Use the Router on the sub route /movies
+app.use(movies.routes());
+
+app.listen(3000);
